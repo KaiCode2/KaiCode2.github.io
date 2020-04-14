8a819b3969cd175631d1d0165b7654a3692ffa61
Kai Aldag <kaialdag@icloud.com>
2020-04-14 16:15:01 -0700

    remade pages

---
+++ index.md
@@ -0,0 +1,5 @@
+---
+layout: default
+---
+# Kai Aldag
+welcome to my Site.
--- post.py
+++
@@ -1,8 +0,0 @@
-
-class Post(object):
-    """Class for blog posts."""
-    def __init__(self, header, brief, link):
-        super(Post, self).__init__()
-        self.header = header
-        self.brief = brief
-        self.link = link
